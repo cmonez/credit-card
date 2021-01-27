e74466119d15c9fcd9ed34e92a45407917a3c37c
Eric Zepeda <zepedaeric@gmail.com>
2021-01-26 22:32:03 -0800

    Begin form container styling

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyle from './styles/globalStyles';
-
+import FormContainer from './styles/Form';
 function App() {
   return (
     <Fragment>
       <GlobalStyle />
       <h1>Credit</h1>
+      <FormContainer></FormContainer>
     </Fragment>
   );
 }
---
+++ src/styles/Form.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const FormContainer = styled.div`
+  font-size: 1.5em;
+  text-align: center;
+  color: white;
+  height: 100px;
+  width: 50px;
+  background-color: white;
+  position: absolute;
+  top: 35%;
+  left: 50%;
+`;
+
+export default FormContainer;
