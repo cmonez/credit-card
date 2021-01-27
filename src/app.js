95761c21ed93821f9f6e2f916f5128807ada423c
Eric Zepeda <zepedaeric@gmail.com>
2021-01-26 22:17:29 -0800

    Create global styles

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import GlobalStyle from './styles/globalStyles';
 
-const template = <p>Credit Card!</p>;
-
-ReactDOM.render(template, document.getElementById('root'));
+function App() {
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <h1>Credit</h1>
+    </Fragment>
+  );
+}
+ReactDOM.render(<App />, document.getElementById('root'));
